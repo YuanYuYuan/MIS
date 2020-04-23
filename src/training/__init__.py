198226578eab708796066c647413ef8e306ec9be
yuanyuyuan <az6980522@gmail.com>
2020-04-24 05:55:51 +0800

    fix bug

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -3,4 +3,4 @@ from .model_handler import ModelHandler
 from .optimizers import Optimizer
 from .runner import Runner
 from .scheduler import Scheduler
-from .learner import Learner
+from .learners import *
